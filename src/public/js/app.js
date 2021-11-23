e6db9b41e7485571b90780dd73f16c8c654f79e5
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-23 17:07:10 +0900

    socket.io로 프론트와 백 소통 쉽게 하기 예제

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,7 +6,9 @@ const form = welcome.querySelector("form");
 const handleRoomSubmit = (event) => {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value });
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
   input.value = "";
 };
 
