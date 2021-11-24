0bb121ea299bf8e72a254ad737cccca90669fd50
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-24 23:22:33 +0900

    대화방 참여/떠날시 카운트기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -46,11 +46,15 @@ const handleRoomSubmit = (event) => {
 
 enter.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", (user) => {
+socket.on("welcome", (user, newCount) => {
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName} (${newCount})`;
   addMessage(`${user} Joind!`);
 });
 
-socket.on("bye", (left) => {
+socket.on("bye", (left, newCount) => {
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName} (${newCount})`;
   addMessage(`${left} left...`);
 });
 
