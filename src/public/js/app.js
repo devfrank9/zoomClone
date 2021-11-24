ea991097f12f7e05a2e98cb19f45dcba041201b4
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-24 14:17:42 +0900

    입력한 메세지 출력하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,11 +15,23 @@ const addMessage = (message) => {
   ul.appendChild(li);
 };
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
 const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 };
 
 const handleRoomSubmit = (event) => {
@@ -39,3 +51,5 @@ socket.on("welcome", () => {
 socket.on("bye", () => {
   addMessage("Someone left...");
 });
+
+socket.on("new_message", addMessage);
