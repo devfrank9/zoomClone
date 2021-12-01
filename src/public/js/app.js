ef95f18df227a85e7d302644e05517f153653611
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-01 16:59:13 +0900

    기기의 비디오 불러와서 웹페이지에 띄우는기능, 음소거/영상가리기 버튼 생성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,46 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  if (!cameraOff) {
+    cameraBtn.innerText = "Turn Camera ON";
+    cameraOff = true;
+  } else {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
